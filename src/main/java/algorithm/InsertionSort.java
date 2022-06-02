6a1f4efe5deb49bcaaf40fdb7851ed101bf40172
ddh963963 <ddh963963@naver.com>
2022-06-02 18:59:51 +0900

    Feat : Bubble,Insertion,Quick,Select Sort Test

---
+++ src/main/java/algorithm/InsertionSort.java
@@ -0,0 +1,28 @@
+package algorithm;
+
+public class InsertionSort {
+
+
+    public static void main(String[] args) {
+
+        final int[] arr = {1, 4, 5, 3, 2, 6, 8, 7, 9, 10, 12, 11, 15, 13, 12, 23, 54, 33, 44, 55, 66, 16, 53, 66, 74, 65};
+        int temp, i, j;
+
+        for (i = 0; i < arr.length - 1; i++) {
+            j = i;
+            System.out.println("현재 선택한 값 arr["+i+"] = "+ arr[i]);
+
+            while (arr[j] > arr[j + 1]) {
+                System.out.println("현재 j 값 = " + j);
+                System.out.println("비교 arr["+j+"] = "+ arr[j]+">  arr["+ arr[j + 1]+"] = "+arr[j+1] );
+                temp =arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                j--;
+            }
+
+
+        }
+
+    }
+}
---
+++ src/main/java/algorithm/QuickSort.java
@@ -0,0 +1,53 @@
+package algorithm;
+
+public class QuickSort {
+    /**
+     * 특정한 값을 기준을 통해서 나누는 알고리즘
+     * 첫번째 값으로 피벗을 설정함
+     */
+
+
+    final static int[] arr = {1, 10, 4, 12, 2, 13, 3, 9, 8, 6};
+
+    public static void main(String[] args) {
+
+
+        quickSort(arr.length - 1, 0);
+        for (int i : arr) {
+            System.out.println("i = " + i);
+        }
+
+    }
+
+    private static void quickSort(int end, int start) {
+        if (start >= end) {
+            return;
+        }
+        int i = start + 1;//왼쪽출발
+        int j = end; //오른쪽출발
+        int key = start;
+        int temp;
+
+
+        while (i <= j) {
+            while ( i <= end && arr[i] <= arr[key] ) {
+                i++;
+            }
+            while (arr[key] <= arr[j] && j > start) {
+                j--;
+            }
+            if (i > j) {
+                temp = arr[j];
+                arr[j] = arr[key];
+                arr[key] = temp;
+            } else {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        quickSort(j - 1, start);
+        quickSort(end, j + 1);
+
+    }
+}
