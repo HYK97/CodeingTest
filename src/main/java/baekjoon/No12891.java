cb9e683ee63ffd53f3d70c0e19e868dadaca04de
ddh963963 <ddh963963@naver.com>
2022-07-03 19:19:55 +0900

    Fix : 백준 DNA 비밀번호 if문 수정

--- src/main/java/baekjoon/No12891.java
+++ src/main/java/baekjoon/No12891.java
@@ -1,43 +1,47 @@
 package baekjoon;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class No12891 {
+    static Map<Character, Integer> check = new HashMap<>();
+    static char[] data;
+    static Map<Character, Integer> now = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int S = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        char[] data = st.nextToken().toCharArray();
+        data = st.nextToken().toCharArray();
         st = new StringTokenizer(br.readLine());
-        Map<Character, Integer> check = new HashMap<>();
         check.put('A', Integer.parseInt(st.nextToken()));
         check.put('C', Integer.parseInt(st.nextToken()));
         check.put('G', Integer.parseInt(st.nextToken()));
         check.put('T', Integer.parseInt(st.nextToken()));
-        Map<Character, Integer> now = new HashMap<>();
         now.put('A', 0);
         now.put('C', 0);
         now.put('G', 0);
         now.put('T', 0);
+
         int count = 0;
 
         for (int i = 0; i < P; i++) {
-            add(i, data, now);
+            add(i);
         }
-        if (check(check, now)) {
+        if (check()) {
             count++;
         }
         for (int i = P; i < S; i++) {
             int j = i - P;
-            add(i, data, now);
-            remove(data, now, j);
-            if (check(check, now)) {
+            add(i);
+            remove(j);
+            if (check()) {
                 count++;
             }
         }
@@ -46,7 +50,7 @@ public class No12891 {
 
     }
 
-    private static void remove(char[] data, Map<Character, Integer> now, int i) {
+    private static void remove(int i) {
         if (data[i] == 'A') {
             now.put('A', now.get('A') - 1);
         } else if (data[i] == 'C') {
@@ -58,30 +62,28 @@ public class No12891 {
         }
     }
 
-    private static boolean check(Map<Character, Integer> check, Map<Character, Integer> now) {
+    private static boolean check() {
         int flag = 0;
         for (Character data : now.keySet()) {
-            if (now.get(data) > 0) {
+            if (now.get(data) >= check.get(data)) {
                 flag++;
             }
         }
-        if (check.equals(now) || flag == 0) {
+        if (flag == 4) {
             return true;
         } else {
             return false;
         }
-
-
     }
 
-    private static void add(int P, char[] data, Map<Character, Integer> now) {
-        if (data[P] == 'A') {
+    private static void add(int i) {
+        if (data[i] == 'A') {
             now.put('A', now.get('A') + 1);
-        } else if (data[P] == 'C') {
+        } else if (data[i] == 'C') {
             now.put('C', now.get('C') + 1);
-        } else if (data[P] == 'G') {
+        } else if (data[i] == 'G') {
             now.put('G', now.get('G') + 1);
-        } else if (data[P] == 'T') {
+        } else if (data[i] == 'T') {
             now.put('T', now.get('T') + 1);
         }
     }
