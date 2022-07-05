7e7ddc6a9db552c841bbf5dcdf7a02f4b2308f6b
ddh963963 <ddh963963@naver.com>
2022-07-05 18:48:18 +0900

    Feat : 1874 stack 풀이

---
+++ src/main/java/baekjoon/No1874.java
@@ -0,0 +1,56 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class No1874 {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] data = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            data[i] = Integer.parseInt(br.readLine());
+        }
+        int i = 1;
+        int j = 1;
+        int count = N * 2;
+        do {
+            if (stack.isEmpty()) {
+                sb.append("+\n");
+                stack.push(i);
+                i++;
+                count--;
+            }
+            if (stack.peek() == data[j]) {
+                data[j] = 0;
+                stack.pop();
+                sb.append("-\n");
+                j++;
+                count--;
+            } else {
+                sb.append("+\n");
+                stack.push(i);
+                i++;
+                count--;
+            }
+        }
+        while (count != 0);
+        int flag = 0;
+        for (int datum : data) {
+            if (datum != 0) flag = 1;
+        }
+        if (flag == 0) {
+            System.out.println(sb);
+        } else {
+            sb.setLength(0);
+            sb.append("NO");
+            System.out.println(sb);
+        }
+    }
+
+
+}
