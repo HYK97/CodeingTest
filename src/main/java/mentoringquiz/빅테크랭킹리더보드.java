8dbef70fae89f33680faab7cb25c11a3699ab2bf
ddh963963 <ddh963963@naver.com>
2022-07-29 11:17:49 +0900

    Feat : tree 순회로 변경

--- src/main/java/mentoringquiz/빅테크랭킹리더보드.java
+++ src/main/java/mentoringquiz/빅테크랭킹리더보드.java
@@ -1,13 +1,10 @@
 package mentoringquiz;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.SortedMap;
+import java.util.Comparator;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class 빅테크랭킹리더보드 {
-    SortedMap<Integer, String> rankingChart = Collections.synchronizedSortedMap(new TreeMap<>(Collections.reverseOrder()));
+    TreeMap<Integer, String> rankingChart = new TreeMap<>(Comparator.reverseOrder()); ///(new TreeMap<>(Collections.reverseOrder()));
 
     public static void main(String[] args) {
         빅테크랭킹리더보드 t = new 빅테크랭킹리더보드();
@@ -16,7 +13,7 @@ public class 빅테크랭킹리더보드 {
         for (int i = 0; i < 4; i++) {
             t.putUser(name[i], score[i]);
         }
-        System.out.println(t.getName(2));
+        System.out.println(t.getName(1));
         System.out.println(t.getRank("d"));
     }
 
@@ -35,11 +32,19 @@ public class 빅테크랭킹리더보드 {
     }
 
     public String getName(int rank) {
-        if (rank == 0) {
+        if (rank == 0 || rankingChart.size() < rank) {
             throw new IllegalArgumentException("잘못된인수");
         }
-        List<String> values = rankingChart.values().stream().collect(Collectors.toList());
-        return values.get(rank - 1);
+        int count = 1;
+        String name = null;
+        for (Integer integer : rankingChart.keySet()) {
+            if (count == rank) {
+                name = rankingChart.get(integer);
+            }
+            count++;
+        }
+
+        return name;
     }
 
     public void putUser(String name, int score) {
