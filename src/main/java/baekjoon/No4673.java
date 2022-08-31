46a12f4e9059b3aa763c557d874089eaded2b24e
HYKim <ddh963963@naver.com>
2022-08-31 13:01:00 +0900

    Fix : 백준 4673 문제 수정

--- src/main/java/baekjoon/No4673.java
+++ src/main/java/baekjoon/No4673.java
@@ -1,33 +1,38 @@
 package baekjoon;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class No4673 {
-    public static void main(String[] args) {
-        System.out.println("1");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] data = new int[5];
-
-        for (int i = 0; i < 10000; i++) {
-            String sdata = String.valueOf(data);
-            for (int j = 0; j < sdata.length(); j++) {
-                int result = j;
-                result += sdata.charAt(i) - '0';
+        for (int i = 1; i < 10000; i++) {
+            if (check(i)) {
+                bw.write(i + "\n");
             }
         }
+        bw.flush();
+        bw.close();
 
 
-        sum(1);
     }
 
-    public static void sum(int data) {
-        System.out.println(data);
-        String sdata = String.valueOf(data);
-        int[] arrayData = new int[5];
-        int result = data;
-        for (int i = 0; i < sdata.length(); i++) {
-            result += sdata.charAt(i) - '0';
+    public static boolean check(int data) {
+        char[] chars;
+        int result;
+        for (int i = 1; i < data; i++) {
+            result = i;
+            chars = String.valueOf(i).toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                result += chars[j] - '0';
+            }
+            if (result == data) {
+                return false;
+            }
+            chars = null;
         }
-        if (result < 10000) {
-            sum(result);
-        }
+        return true;
     }
 }
