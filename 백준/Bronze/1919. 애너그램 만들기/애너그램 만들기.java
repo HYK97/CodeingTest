9300541d7c2f75cd2f6a7e140d844015a53101f3
HYKim <ddh963963@naver.com>
2023-11-28 23:15:01 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,35 +1,26 @@
-import java.io.*;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
+
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        LinkedHashMap<Character, Integer> firstMap = new LinkedHashMap<>();
-        LinkedHashMap<Character, Integer> secondMap = new LinkedHashMap<>();
         char[] first = br.readLine().toCharArray();
         char[] second = br.readLine().toCharArray();
-
-        for (char c = 'a'; c <= 'z'; c++) {
-            firstMap.put(c, 0);
-            secondMap.put(c, 0);
-        }
-
+        int[] alp1 = new int[26];
+        int[] alp2 = new int[26];
         for (char c : first) {
-            firstMap.put(c, firstMap.get(c) + 1);
+            alp1[c - 97]++;
         }
         for (char c : second) {
-            secondMap.put(c, secondMap.get(c) + 1);
+            alp2[c - 97]++;
         }
-        int result=0;
-        for (Character c : firstMap.keySet()) {
-            Integer firstData = firstMap.get(c);
-            Integer secondData = secondMap.get(c);
-            if (!firstData.equals(secondData)) {
-                result+=Math.abs(firstData-secondData);
+        int result = 0;
+        for (int i = 0; i < 26; i++) {
+            int one = alp1[i];
+            int two = alp2[i];
+            if (one != two) {
+                result += Math.abs(one - two);
             }
         }
         System.out.println(result);
