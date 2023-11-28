0db35faec0049940147d6c32c4d3aa0e0fa1c56c
HYKim <ddh963963@naver.com>
2023-11-28 22:31:56 +0900

    [Bronze II] Title: Strfry, Time: 272 ms, Memory: 22628 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -1,35 +1,38 @@
-import java.util.*;
-import java.util.stream.Collectors;
+
+import java.io.*;
+import java.util.Arrays;
 
 
 public class Main {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = Integer.parseInt(br.readLine());
         for (int i = 0; i < count; i++) {
             char[] alp1=new char[26];
             char[] alp2=new char[26];
-            char[] original = sc.next().toCharArray();
-            char[] target = sc.next().toCharArray();
+            String []s = br.readLine().split(" ");
+            char[] original = s[0].toCharArray();
+            char[] target = s[1].toCharArray();
             for (char c : original) {
-
                 alp1[c-97]++;
             }
             for (char c : target) {
-
                 alp2[c-97]++;
             }
             boolean flag =true;
             for (int j = 0; j < 26; j++) {
                 if (alp1[j] != alp2[j]) {
-                    System.out.println("Impossible");
+                    bw.write("Impossible");
                     flag=false;
                     break;
                 }
             }
             if (flag) {
-                System.out.println("Possible");
+                bw.write("Possible");
             }
+            bw.newLine();
         }
+        bw.close();
     }
 }
