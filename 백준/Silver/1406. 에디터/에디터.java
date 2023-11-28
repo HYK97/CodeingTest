00628d6be6022a89526618c121622313efdb28f2
HYKim <ddh963963@naver.com>
2023-11-28 23:57:13 +0900

    [Silver II] Title: 에디터, Time: 860 ms, Memory: 163944 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -0,0 +1,43 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        LinkedList<String> init = Arrays.stream(br.readLine().split("")).collect(Collectors.toCollection(LinkedList::new));
+        LinkedList<String> buffer = new LinkedList<>();
+        int numberOfCommands = Integer.parseInt(br.readLine());
+        for (int i = 0; i < numberOfCommands; i++) {
+            String command = br.readLine();
+            if (command.equals("L") && !init.isEmpty()) {
+                buffer.addFirst(init.pollLast());
+                continue;
+            }
+            if (command.equals("D") && !buffer.isEmpty()) {
+                init.addLast(buffer.pollFirst());
+                continue;
+            }
+            if (command.equals("B") && !init.isEmpty()) {
+                init.removeLast();
+                continue;
+            }
+            String[] pCommand = command.split(" ");
+            if (pCommand.length == 2) {
+                String addData = pCommand[1];
+                init.addLast(addData);
+            }
+        }
+        for (String s : init) {
+            bw.write(s);
+        }
+        for (String s : buffer) {
+            bw.write(s);
+        }
+        bw.newLine();
+        bw.close();
+    }
+}
