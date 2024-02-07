ea94ad12b502b2715a1a10f27cf6422dac35cbf5
HYKim <ddh963963@naver.com>
2024-02-07 22:43:20 +0900

    [Silver II] Title: 종이의 개수, Time: 1076 ms, Memory: 320548 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static Result result = new Result();
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		}
+		recursion(map, 0, 0, N);
+		System.out.println(result.minusOne);
+		System.out.println(result.zero);
+		System.out.println(result.one);
+
+	}
+
+	private static void recursion(int[][] map, int x, int y, int N) {
+		if (check(map, x, y, N)) {
+			cal(map[x][y]);
+			return;
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				recursion(map, i * N / 3 + x, j * N / 3 + y, N / 3);
+			}
+		}
+
+
+	}
+
+	private static void cal(int data) {
+		if (data == 0) {
+			result.zero++;
+			return;
+		}
+		if (data == 1) {
+			result.one++;
+			return;
+		}
+		if (data == -1) {
+			result.minusOne++;
+		}
+	}
+
+
+	public static boolean check(int[][] map, int x, int y, int N) {
+		int checkValue = map[x][y];
+		for (int i = x; i < x + N; i++) {
+			for (int j = y; j < y + N; j++) {
+				int data = map[i][j];
+				if (checkValue != data) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	static class Result {
+		int zero;
+		int one;
+		int minusOne;
+
+		public Result() {
+			this.zero = 0;
+			this.one = 0;
+			this.minusOne = 0;
+		}
+	}
+
+}
