dc820adbb674fcabe1d6e6c092cca90732d946d8
HYKim <ddh963963@naver.com>
2024-02-23 23:39:40 +0900

    feat: code update

---
+++ src/main/java/baekjoon/backtraking/No15650.kt
@@ -0,0 +1,23 @@
+package baekjoon.backtraking
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (N, M) = br.readLine().split(" ").map(String::toInt)
+    val check = IntArray(M)
+
+    fun backtracking(index: Int, count: Int) {
+        if (count == M) {
+            for (i in check) {
+                print("${i} ")
+            }
+            println()
+            return
+        }
+        for (i in index until N) {
+            check[count] = i + 1
+            backtracking(i + 1, count + 1)
+            check[count] = 0
+        }
+    }
+    backtracking(0, 0)
+}
