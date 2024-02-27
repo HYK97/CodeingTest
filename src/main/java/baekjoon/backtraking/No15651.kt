06cba972447a252dd2f9a0722c837c1ee2353528
HYKim <ddh963963@naver.com>
2024-02-27 14:45:53 +0900

    feat: code update

---
+++ src/main/java/baekjoon/backtraking/No15651.kt
@@ -0,0 +1,30 @@
+package baekjoon.backtraking
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val readLine = br.readLine().split(" ")
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val N = readLine[0].toInt()
+    val M = readLine[1].toInt()
+    val list = LinkedList<Int>()
+    fun backtracking(N: Int, M: Int, index: Int) {
+        if (index == M) {
+            bw.write(list.joinToString(separator = " ")+"\n")
+            return
+        }
+        for (i in 1..N) {
+            list.addLast(i)
+            backtracking(N, M, index + 1)
+            list.pollLast()
+        }
+    }
+    backtracking(N, M, 0)
+    bw.flush()
+    bw.close()
+}
