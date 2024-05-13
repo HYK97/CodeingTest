537122138df4eaf08dbf34ec8eb83af4bed0794f
HYKim <ddh963963@live.wsu.ac.kr>
2024-05-13 23:45:29 +0900

    Time: 238 ms (50%), Space: 39.9 MB (50%) - LeetHub

---
+++ leetcode/0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.kt
@@ -0,0 +1,53 @@
+class Solution {
+  fun matrixScore(grid: Array<IntArray>): Int {
+        for (ints in grid.withIndex()) {
+            val original = ints.value.joinToString("")
+            val originalInt = original.toInt(2)
+            val reverse = invertBinaryString(original)
+            val reverseInt = reverse.toInt(2)
+            if (originalInt > reverseInt) {
+                continue
+            }
+            if (reverseInt > originalInt) {
+                grid[ints.index] = reverse.chunked(1).map { it.toInt() }.toIntArray()
+            }
+        }
+
+
+         for (row in grid[0].withIndex()) {
+            var original = ""
+            for (col in grid.withIndex()) {
+                original += grid[col.index][row.index].toString()
+            }
+
+            val originalInt = original.count { it == '1' }
+            val reverse = invertBinaryString(original)
+            val reverseInt = reverse.count { it == '1' }
+            if (originalInt > reverseInt) {
+                continue
+            }
+
+            if (reverseInt > originalInt) {
+                val toIntArray = reverse.chunked(1).map { it.toInt() }.toIntArray()
+                for (col in grid.withIndex()) {
+                    grid[col.index][row.index] = toIntArray[col.index]
+                }
+            }
+
+        }
+
+        var sum = 0
+        for (ints in grid) {
+            sum += ints.joinToString("").toInt(2)
+        }
+        return sum
+    }
+
+    fun invertBinaryString(binary: String): String {
+        val sb = StringBuilder(binary.length)
+        for (char in binary) {
+            sb.append(if (char == '0') '1' else '0')
+        }
+        return sb.toString()
+    }
+}
