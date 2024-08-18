448a469ea0e629977bdd3859565483813cd635ec
HYKim <ddh963963@live.wsu.ac.kr>
2024-08-18 15:01:01 +0900

    Time: 135 ms (95.43%), Space: 36.5 MB (24.46%) - LeetHub

---
+++ leetcode/1768-merge-strings-alternately/1768-merge-strings-alternately.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val word1s = word1.toCharArray()
+        val word2s = word2.toCharArray()
+        var mergeData =""
+        val max = max(word1s.size, word2s.size)
+        for (i in 0 until max) {
+            if (i < word1s.size) {
+                mergeData += word1s[i]
+            }
+            if (i < word2s.size) {
+                mergeData += word2s[i]
+            }
+        }
+        return mergeData
+    }
+}
