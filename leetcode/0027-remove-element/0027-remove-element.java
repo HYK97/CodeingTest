6c056de35892e4646cc907242c2e48205ede2bd0
김현용 <ddh963963@live.wsu.ac.kr>
2026-01-07 17:19:58 +0900

    N/A (0%) N/A (0%) 01-07-2026 0027-remove-element Easy .java Array

--- leetcode/0027-remove-element/0027-remove-element.java
+++ leetcode/0027-remove-element/0027-remove-element.java
@@ -1,42 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int left = 0;
-        int right = nums.length - 1;
-        int count = 0;
-
-        if (nums.length == 1 && val == nums[0]){
-            return 0;
-        } else if(nums.length == 1 && val != nums[0]) {
-            return 1;
-        }
-
-        while (left <= right) {
-            int target = nums[left];
-            if(target != val){
-                count++;
-                left++;
-                continue;
+        int right=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[right]=nums[i];
+                right++;
             }
-            while(val == nums[right] && left < right){
-                right--;
-            }
-            if(left >= right){
-                break;
-            }
-            int temp = nums[right];
-            nums[left]= temp;
-            nums[right]= target;
-            count++;
-            left++;
-
         }
-        
-        for(int num : nums){
-            System.out.println("array : " + num);
-        }
-
-        System.out.println("test : " + count);
-        return count;
-    }
-    
+        return k;
 }
