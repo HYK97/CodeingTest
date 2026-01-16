4676401c74c6e97620ea863656de4d7a5e6cfa70
김현용 <ddh963963@live.wsu.ac.kr>
2026-01-16 18:04:34 +0900

    2 ms (94.27%) 72.3 MB (19.34%) 01-16-2026 0238-product-of-array-except-self Medium .java Array

--- leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int [] result = new int [nums.length];
-        int tmp = 0;
+        int tmp = 1;
         for (int i = 0 ; i < nums.length ; i++){
             if (i == 0){
                 result[i] = 1;
@@ -10,11 +10,7 @@ class Solution {
             result[i] = result[i-1] * nums[i-1];
         }
 
-        for (int i = nums.length - 1 ; i >= 0 ; i--){
-            if (i == nums.length - 1){
-                tmp = 1;
-                continue;
-            }
+        for (int i = nums.length - 2 ; i >= 0 ; i--){
             tmp = tmp * nums[i+1];
             result[i] = tmp * result[i];
         }
