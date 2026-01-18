b526652af9ec6ff4bc1e119b1bccb0b7d9b7d075
김현용 <ddh963963@live.wsu.ac.kr>
2026-01-18 16:45:41 +0900

    4 ms (92.11%) 45.1 MB (94.86%) 01-18-2026 0013-roman-to-integer Easy .kt Hash Table

--- leetcode/0013-roman-to-integer/0013-roman-to-integer.kt
+++ leetcode/0013-roman-to-integer/0013-roman-to-integer.kt
@@ -1,39 +1,26 @@
 class Solution {
     fun romanToInt(s: String): Int {
         var sum = 0
-        val splits = s.split("")
-        var prev = ""
-        for (split in splits) {
-            if (prev == "") {
-                prev = split
-                continue
-            }
-            val change = change(prev + split)
-            if (change != 0) {
-                sum += change
-                prev = ""
+        val length = s.length
+        for (i in 0 until length) {
+            val currentVal = getValue(s[i])
+            if (i < length - 1 && currentVal < getValue(s[i + 1])) {
+                sum -= currentVal
             } else {
-                sum += change(prev)
-                prev = split
+                sum += currentVal
             }
         }
         return sum
     }
 
-    fun change(s: String) = when (s) {
-        "I" -> 1
-        "IV" -> 4
-        "V" -> 5
-        "IX" -> 9
-        "X" -> 10
-        "XL" -> 40
-        "L" -> 50
-        "XC" -> 90
-        "C" -> 100
-        "CD" -> 400
-        "D" -> 500
-        "CM" -> 900
-        "M" -> 1000
+    fun getValue(c: Char) = when (c) {
+        'I' -> 1
+        'V' -> 5
+        'X' -> 10
+        'L' -> 50
+        'C' -> 100
+        'D' -> 500
+        'M' -> 1000
         else -> 0
     }
 }
