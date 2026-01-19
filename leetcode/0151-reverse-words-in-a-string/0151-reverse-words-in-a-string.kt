be2b1cf35210c00aeb28c8eecabede6787079efd
김현용 <ddh963963@live.wsu.ac.kr>
2026-01-19 22:08:22 +0900

    26 ms (15.38%) 47.4 MB (5.83%) 01-19-2026 0151-reverse-words-in-a-string Medium .kt Two Pointers

--- leetcode/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
+++ leetcode/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
@@ -1,17 +1,19 @@
 class Solution {
     fun reverseWords(s: String): String {
-        val replaceSpace = replaceSpace(s)
-        replaceSpace.reverse()
-        val joinToString = replaceSpace.joinToString(" ")
-        return joinToString
-    }
-
-    private fun replaceSpace(s: String): MutableList<String> {
-        val split = s.split(" ")
-        val replaceList = split
-            .asSequence()
-            .filter { it != "" }
-            .toMutableList()
-        return replaceList
+        val result = StringBuilder()
+        val replace = s.trim().replace("\\s+".toRegex(), " ")
+        var data = ""
+        for (i in replace) {
+            if (i != ' ') {
+                data += i
+            } else {
+                result.insert(0," "+data)
+                data = ""
+            }
+        }
+        if (data.isNotEmpty()){
+            result.insert(0, data)
+        }
+        return result.toString()
     }
 }
