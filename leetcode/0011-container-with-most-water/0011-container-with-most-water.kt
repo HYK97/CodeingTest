47fd10702dfc33068e15c9a4a6733d12cfa2cfaa
김현용 <ddh963963@live.wsu.ac.kr>
2026-01-23 13:38:11 +0900

    6 ms (80.11%) 59.8 MB (74.41%) 01-23-2026 0011-container-with-most-water Medium .kt Array

--- leetcode/0011-container-with-most-water/0011-container-with-most-water.kt
+++ leetcode/0011-container-with-most-water/0011-container-with-most-water.kt
@@ -1,23 +1,22 @@
+// 양쪽에서 시작 계산해서 max값 갱신
+// 단 벽이 작은쪽이 움직이는 식으로 진행
 class Solution {
     fun maxArea(height: IntArray): Int {
-        var maxArea = -1
-
-        // 높이 결정
-        var left = 0
-        var right = height.size - 1
-        //밑변
-        var base = height.size - 1
-        while (left != right) {
-            val minHeight = min(height[left], height[right])
-            val area = minHeight * base
-            maxArea = max(maxArea, area)
-            if (height[left] < height[right]) {
-                left++
-            } else {
-                right--
-            }
-            base--
+        var maxSize = 0
+        var p1 = 0
+        var p2 = height.size - 1
+        while(p1 < p2){
+            var left = height[p1]
+            var right = height[p2]
+            var currentHeight = Math.min(left,right)
+            var currentSize = currentHeight * (p2-p1)
+            maxSize = Math.max(maxSize,currentSize)
+            if(left>right){
+                p2--
+            } else{
+                p1++
+            }            
         }
-        return maxArea
+        return maxSize
     }
 }
